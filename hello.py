063efc88fc60ed6a54d8ec177df33ff289423098
drenxhyliqi <xhyliqiidren@gmail.com>
2026-03-24 15:26:17 +0100

    comments in python

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 print("Hello World!")
 print("Welcome to Python programming.")
 print("Setting Up Python enviroment")
+
+# This is a comment in python
+# Comments are used to explain code and make it more readable
+# Comments are ignored by the Python interpreter
+
+
+# Printing multiple lines
+print("This is the first line.")
+print("This is the second line.")
+print("This is the third line.")
