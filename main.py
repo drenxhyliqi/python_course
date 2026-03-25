cfc7ed6d1e188242e76d9f2a636060492e641c20
drenxhyliqi <xhyliqiidren@gmail.com>
2026-03-25 13:08:13 +0100

    simple program filtering phone numbers from other signs

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ if len(password) < 8:
     print("Password should have 8 or more characters")
 else:
     print("Passowrd is the right length!")
+
+# Filter and find only the number without any operators
+phone = "+383-49-211-199"
+
+print(
+    f"The phone number filtered is: {phone[phone.find('+'):].replace('-', '').strip('+')}"
+)
