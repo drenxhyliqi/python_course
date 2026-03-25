593311554be2ef07f4e81c1d9144551878d87961
drenxhyliqi <xhyliqiidren@gmail.com>
2026-03-25 21:34:00 +0100

    numeric operations and math functions

--- numbers.py
+++ numbers.py
@@ -1,3 +1,6 @@
+import math
+
+
 def sep():
     print("-" * 30)
 
@@ -22,6 +25,31 @@ print(type(d_as_num))  # Now the d is converted to an int number
 print(d_as_num * 3)  # It multiplys the number
 sep()
 
-
 # We can also convert a number from int to float
 print(float(d_as_num))
+
+sep()
+
+# Numeric Operators
+print(2 + 3)
+print(9 - 3)
+print(5 * 5)
+print(10 / 2)
+print(10 // 3)  # It rounds the number
+print(7 % 3)  # It prints the leftovers after the number is divided
+print(2 ** 10)  # It prints the first number on the power of the second power
+
+sep()
+
+# Math functions
+print(abs(2 - 10))  # The abs function removes the negative sign
+# Rounding Numbers
+price = 321.3841
+print(round(price))
+print(round(price, 2))
+# This is not a built in function you have to import the math python library
+print(math.floor(price))
+print(math.ceil(price))
+
+# Math trunc removes the other numbers does not round the number
+print(math.trunc(price))
