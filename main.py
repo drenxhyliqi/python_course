e2277720169b078d13b69e1b19edf73b167ff818
drenxhyliqi <xhyliqiidren@gmail.com>
2026-03-25 21:58:37 +0100

    generate and check iseven challenge

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 # First program with variables, data types, inputs and an simple if statement
 password = input("Enter your password: ")
 if len(password) < 8:
@@ -11,3 +12,12 @@ phone = "+383-49-211-199"
 print(
     f"The phone number filtered is: {phone[phone.find('+'):].replace('-', '').strip('+')}"
 )
+
+# Generate a random number from 1-100 and check if that number is even
+random_num = random.randint(1, 100)
+print(random_num)
+
+if random_num % 2 == 0:
+    print(f"The number {random_num} is even")
+else:
+    print(f"The number {random_num} is odd")
