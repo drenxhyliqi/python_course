db3306ffaeb090c09cc5408d24bbd8590a4cc7a5
drenxhyliqi <xhyliqiidren@gmail.com>
2026-03-29 22:45:24 +0200

    parameters and arguments

--- functions.py
+++ functions.py
@@ -5,9 +5,14 @@ def make_coffee():
     print("Add Milk")
     print("Enyoy it")
 
+# *args and **kwargs allow functions to accpet a unknown number of arguments
 
-def clean_up(name):
-    print(name.strip().lower())
+
+def clean_up(first_name, last_name, country="Not Known"):  # Default Parameters
+    first = first_name.strip().lower()
+    last = last_name.strip().lower()
+    full_name = first + " " + last
+    print(full_name, "From: ", country)
 
 
 print("Wake up")
@@ -19,5 +24,6 @@ make_coffee()
 name = "  MariA  "
 print(name.strip().lower())
 
-clean_up("  LarRrED ")
-clean_up("  SaaDerr ")
+clean_up("  LarRrED ", " JacKSon  ", "DE")  # Positional Argument
+clean_up("  SaaDerr ", "  KoldAn  ", "USA")
+clean_up("  DreN ", "  XhyLIQI  ")
