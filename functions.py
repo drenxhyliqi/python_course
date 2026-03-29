037a03880bad33210ece1e370558d59c4eae1934
drenxhyliqi <xhyliqiidren@gmail.com>
2026-03-29 22:29:04 +0200

    introduction to functions

---
+++ functions.py
@@ -0,0 +1,23 @@
+# Functions
+def make_coffee():
+    print("Start Machine")
+    print("Make Coffee")
+    print("Add Milk")
+    print("Enyoy it")
+
+
+def clean_up(name):
+    print(name.strip().lower())
+
+
+print("Wake up")
+make_coffee()
+print("Working for a while")
+make_coffee()
+
+# Params and arguments
+name = "  MariA  "
+print(name.strip().lower())
+
+clean_up("  LarRrED ")
+clean_up("  SaaDerr ")
